2e3513d6d677244a2b17cf65278ad3692ca0974c
Babatunde Olaniyi <bolaniyi.mvc@gmail.com>
2020-04-21 14:22:03 -0500

    blog progress added some pictures

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -9,10 +9,24 @@ import Card from '../UI/Card';
 
 const BlogPost = (props) => {
   return(
+     <div className="blogPostContainer">
+       <Card>
+           <div  className="blogHeader">
+           <span className="blogCategory">Featured</span>
+           <h1 className="postTitle">Beautiful is always beautiful</h1>
+           <span className="postedBy">posted on July21, 2016 bySora blogging Tips</span>
+           </div>
 
-    <Card width="70%">
-     My first blog Post
-      </Card>
+           <div className="postImageContainer">
+            <img src={require('../../blogPostImages/memories-from.jpg')} alt="Post Image"/>
+           </div>
+        <div className="postContent">
+           <h3>Post Title</h3>
+           <p>lorem ipsim</p>
+        </div>
+        
+           </Card>
+         </div>
     
    )
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -9,9 +9,47 @@ import Card from '../UI/Card';
 
 const Sidebar  = (props) => {
   return(
-    <Card width='27%'>
-       Sidebar
-    </Card>
+      <div className="sidebarContainer">
+
+    <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
+       <div className="cardHeader">
+           <span>About Us</span>
+           </div>
+           <div className="profileImageContainer">
+           <img src={require('../../blogPostImages/mypic.jpg')} alt="CEO"/>
+       </div>
+       <div className="cardBody">
+           <p className="personalBio">My name is Babatunde Olaniyi I am a software developer specialization in Front end development.....:)</p>
+       </div>
+     </Card>
+
+     <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
+         <div className="cardHeader">
+         <span>Social Network</span>
+         </div>
+     </Card>
+
+     <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
+         <div className="cardHeader">
+             <span>Recent Posts</span>
+         </div>
+
+         <div className="recentPosts">
+             <div className="recentPost">
+                 <h3>Post Title</h3>
+                 <span>July 21,2018</span>
+             </div>
+          </div>
+
+          <div className="recentPosts">
+             <div className="recentPost">
+                 <h3>Post Title</h3>
+                 <span>July 21,2018</span>
+             </div>
+          </div>
+          
+     </Card>
+    </div>
    )
 
  }
